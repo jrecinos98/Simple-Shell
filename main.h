34185aa65101e023415bcf2c19acdeceff619f4d
Nick <ncduncan@umail.ucsb.eduncduncan@umail.ucsb.edu>
2019-01-15 16:12:42 -0800

    Fixed includes on main

--- main.h
+++ main.h
@@ -7,21 +7,21 @@
 #include <string.h>
 #include <string>
 #include <vector>
-#include "Parser.h"
+#include <sstream>
 
 // Parses the string the user enters and returns it as a vector of tokens
 std::vector<std::string> parse_user_input(std::string& command_string) {
 	std::vector<std::string> command_tokens;
 	std::stringstream stringStream(command_string);
 	std::string line;
-	while(std::getline(stringStream, line)) {
+	while(std::getline(stringStream, line)) {  // Get stringstream as string line
 	    std::size_t prev = 0, pos;
-	    while ((pos = line.find_first_of(" |&<>", prev)) != std::string::npos) {
-	        if (pos > prev){
+	    while ((pos = line.find_first_of(" |&<>", prev)) != std::string::npos) {  // Finds the first of the delimiters
+	        if (pos > prev){  // If position i
 	            command_tokens.push_back(line.substr(prev, pos-prev));
 	        }
 	        prev = pos+1;
-			if(line[pos] != ' '){
+			if(line[pos] != ' '){  // Push token after the command string to preserve order
 				std::string temp_token = "";
 				temp_token += line[pos];
 			 	command_tokens.push_back(temp_token);  // Pushes the token if it is not a space
@@ -31,7 +31,6 @@ std::vector<std::string> parse_user_input(std::string& command_string) {
 	        command_tokens.push_back(line.substr(prev, std::string::npos));
 	    }
 	}
-
 	for(unsigned int i = 0; i < command_tokens.size(); i++){
 		printf("Token: %s\n", command_tokens[i].c_str());
 	}
